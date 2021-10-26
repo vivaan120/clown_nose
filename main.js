c4d9eb1882f26fd7d84edbbbcb6ae62c6628193e
vivaan120 <83453732+vivaan120@users.noreply.github.com>
2021-10-26 19:34:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX = 0;
+noseY = 0;
+
+
+function preload(){
+clown_nose = loadImage('https://i.postimg.cc/4NcJ2m2d/nose.png');
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(300,300);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function draw(){
+image(video,0,0,300,300);
+image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot(){
+    save('your_image.png');
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0) {
+        console.log(results);
+    noseX = results[0].pose.nose.x-15;
+    noseY = results[0].pose.nose.y-10;
+
+
+    console.log("nose x = "+ noseX);
+    console.log("nose y = "+ noseY);
+    }
+}
